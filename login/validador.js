278e8e68822fd7d054a8061c4fc3f768b85f0a8e
Angelahb <angelahb1991@hotmail.com>
2020-01-19 22:49:18 -0300

    commit final

--- login/validador.js
+++ login/validador.js
@@ -1,18 +1,25 @@
 
 var button = document.getElementById('login');
 
+function Redireccionar() {
+	document.location.assign('C:\\Users\\angel\\Documents\\proyectos\\ejercicios-taller-apps\\layout\\index.html');
+}
+
 button.onclick = function() {
    var username = document.getElementById('mail').value;
    var password = document.getElementById('psw').value;
+   var labelText = document.getElementById('msglogin');
 
 
       if(username == "ejemplo@gmail.com" && password == "123456") {
          console.clear();
          console.log("Iniciando sesión…");
-
+         labelText.innerHTML = "";
+         Redireccionar();
       }else{
          console.clear();
          console.log("Email y/o contraseña incorrectos");
+         labelText.innerHTML = "Email y/o contraseña incorrectos";
       }
 
 }
